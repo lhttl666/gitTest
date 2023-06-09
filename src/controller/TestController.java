951f9d5d7f6e14868a4a506b39d68866fc4f0546
lhttl666 <1779196455@qq.com>
2023-06-09 20:52:07 +0800

    first

---
+++ src/controller/TestController.java
@@ -0,0 +1,17 @@
+package controller;
+
+/**
+ * @author 86178
+ */
+public class TestController {
+
+    public static void main(String[] args) {
+        System.out.println("白日依山尽");
+        System.out.println("黄河入海流");
+        System.out.println("欲穷千里目");
+//        System.out.println("更上一层楼");
+//        System.out.println("sdfsdf");
+//        System.out.println("ppppppppp");
+    }
+
+}
---
+++ src/domain/People.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class People {
+    public String name;
+    public String address;
+    public Integer age;
+}
